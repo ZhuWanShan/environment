d4d8b9e296da1f35b74cdbc321a50b036b6918fd
Spark Zhu <szhu.spark@gmail.com>
2016-02-06 08:21:01 +0000

    Tmux install

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-https://github.com/erikw/tmux-powerline
+
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p ~/.tmux && cd ~/.tmux
+
+if [ -d tmux-powerline ]
+then
+    cd tmux-powerline && git pull https://github.com/erikw/tmux-powerline
+else
+    git clone https://github.com/erikw/tmux-powerline     
+fi
+
+cp $BASE/.tmux.conf ~/
+
